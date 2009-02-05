5258305c832a398841f0bce2c36db8c70f677d09
Anthony Heathcoat <aeheathc@gmail.com>
2009-02-05 07:33:08 +0000

    Switch SDTS-reading library from sdts++ to GDAL
    git-svn-id: http://svn.code.sf.net/p/laharplot/code/trunk@37 95a9f313-b2ac-4aab-b0cd-30946b13c7fa

--- src/stream/main.cpp
+++ src/stream/main.cpp
@@ -18,13 +18,89 @@
 */
 
 #include <iostream>
-#include "Dem.h"
+#include <gdal/gdal_priv.h>
+
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "Stream finder" << endl;
-	Dem infile("2345");
+    GDALDataset  *poDataset;
+    GDALAllRegister();
+
+    poDataset = (GDALDataset *) GDALOpen( "D:/Download/1652948.DEM.SDTS/8345CATD.DDF", GA_ReadOnly );
+    if( poDataset == NULL )
+    {
+        cout << "Problem opening!" << endl;
+	}else{
+		cout << "Open successful!" << endl;
+	}
+	
+	    double        adfGeoTransform[6];
+
+    printf( "Driver: %s/%s\n",
+            poDataset->GetDriver()->GetDescription(), 
+            poDataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
+
+    printf( "Size is %dx%dx%d\n", 
+            poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
+            poDataset->GetRasterCount() );
+
+    if( poDataset->GetProjectionRef()  != NULL )
+        printf( "Projection is `%s'\n", poDataset->GetProjectionRef() );
+
+    if( poDataset->GetGeoTransform( adfGeoTransform ) == CE_None )
+    {
+        printf( "Origin = (%.6f,%.6f)\n",
+                adfGeoTransform[0], adfGeoTransform[3] );
+
+        printf( "Pixel Size = (%.6f,%.6f)\n",
+                adfGeoTransform[1], adfGeoTransform[5] );
+	}
+
+	        GDALRasterBand  *poBand;
+        int             nBlockXSize, nBlockYSize;
+        int             bGotMin, bGotMax;
+        double          adfMinMax[2];
+        
+        poBand = poDataset->GetRasterBand( 1 );
+        poBand->GetBlockSize( &nBlockXSize, &nBlockYSize );
+        printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
+                nBlockXSize, nBlockYSize,
+                GDALGetDataTypeName(poBand->GetRasterDataType()),
+                GDALGetColorInterpretationName(
+                    poBand->GetColorInterpretation()) );
+
+        adfMinMax[0] = poBand->GetMinimum( &bGotMin );
+        adfMinMax[1] = poBand->GetMaximum( &bGotMax );
+        if( ! (bGotMin && bGotMax) )
+            GDALComputeRasterMinMax((GDALRasterBandH)poBand, TRUE, adfMinMax);
+
+        printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
+        
+        if( poBand->GetOverviewCount() > 0 )
+            printf( "Band has %d overviews.\n", poBand->GetOverviewCount() );
+
+        if( poBand->GetColorTable() != NULL )
+            printf( "Band has a color table with %d entries.\n", 
+                     poBand->GetColorTable()->GetColorEntryCount() );
+
+
+		float *pafScanline;
+        int   nXSize = poBand->GetXSize(), nYSize = poBand->GetYSize();
+        pafScanline = (float *) CPLMalloc(sizeof(float)*nXSize*nYSize);
+        poBand->RasterIO( GF_Read, 0, 0, nXSize, nYSize, pafScanline, nXSize, nYSize, GDT_Float32, 0, 0 );
+
+		for(int yp = 0; yp<nYSize; yp++)
+		{
+			for(int xp = 0; xp<nXSize; xp++)
+				cout << pafScanline[yp * nXSize + xp] << ",\t";
+			cout << endl;
+		}			  
+					  
+					  
+					  
+					  
+	GDALClose((GDALDatasetH*)poDataset);
 	return 0;
 }
