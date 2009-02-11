aacb4b69772f60d4ac1c3aea5566210558d6d0d9
Anthony Heathcoat <aeheathc@gmail.com>
2009-02-11 05:16:28 +0000

    Implemented all program options, file i/o setup, and DEM reading.
    git-svn-id: http://svn.code.sf.net/p/laharplot/code/trunk@40 95a9f313-b2ac-4aab-b0cd-30946b13c7fa

--- src/stream/main.cpp
+++ src/stream/main.cpp
@@ -18,33 +18,108 @@
 */
 
 #include <iostream>
+#include <sstream>
+#include <string>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/program_options.hpp> 
+#include <boost/thread.hpp>
+
 #include <gdal_priv.h>
 
-
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    GDALDataset  *poDataset;
-    GDALAllRegister();
+	// Declare the supported options.
+	po::options_description desc("Usage: stream [OPTION]...");
+	desc.add_options()
+		("help", "Display this help and exit")
+		("input-file,f", po::value<string>(), "Read topography from input file <arg>")
+		("std-in,i", "Read topography from standard-in. Can't be used with input-file.")
+		("output-file,o", po::value<string>(), "Output to files using the base name <arg>.")
+		("std-out,t", "Output to standard-out. May be used with output-file.")
+		("verbose,v", "Display detailed progress.")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);    
 
-    poDataset = (GDALDataset *) GDALOpen( "D:/Download/1652948.DEM.SDTS/8345CATD.DDF", GA_ReadOnly );
-    if( poDataset == NULL )
-    {
-        cout << "Problem opening!" << endl;
-	}else{
-		cout << "Open successful!" << endl;
+	//Show usage information when the user asks for help.
+	if(vm.count("help"))
+	{
+		cout << desc << "\n";
+		return 0;
 	}
 	
-	    double        adfGeoTransform[6];
+	//check for various contradictory option settings, or missing required options.
+	string optError="";
+	string infile="",outfile="";
+	bool cmdIn=vm.count("std-in");
+	bool cmdOut=vm.count("std-out");
+	bool verbose=vm.count("verbose");
+	fs::ofstream sDem,meta,flowDir,flowTotal;
+	
+	if(vm.count("input-file"))
+	{
+		infile = vm["input-file"].as<string>();
+		if(!fs::exists(infile))
+		{
+			optError = infile + ": No such file or directory\n";
+		}else if(fs::is_directory(infile)){
+			optError = infile + ": Is a directory\n";
+		}
+		if(cmdIn)
+			optError = "can't read from both input-file and cmdIn at once\n";
+	}else{
+		if(!cmdIn)
+			optError = "no input source specified\nTry 'stream --help' for more information.\n";
+	}
+	
+	if(vm.count("output-file"))
+	{
+		outfile = vm["output-file"].as<string>();
+		if(outfile.empty())
+		{
+			optError = "invalid filename\n";
+		}else{
+			sDem.open(outfile+"-sdem.tsv");
+			meta.open(outfile+".ini");
+			flowDir.open(outfile+"-fdir.tsv");
+			flowTotal.open(outfile+"-ftotal.tsv");
+			if(!(sDem && meta && flowDir && flowTotal))
+				optError = string("couldn't open output files\n(Is the filename valid?)")
+							+"\n(Is there a permissions issue?)\n";
+		}
+	}else{
+		if(!cmdOut)
+			optError = "no output method specified\nTry 'stream --help' for more information.\n";
+	}
 
-    printf( "Driver: %s/%s\n",
-            poDataset->GetDriver()->GetDescription(), 
-            poDataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
+	if(optError != "")
+	{
+		cout << "stream: " << optError << "\nTry 'stream --help' for more information.\n";
+		return 1;
+	}
 
-    printf( "Size is %dx%dx%d\n", 
-            poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
-            poDataset->GetRasterCount() );
+	//Done setting up. Now, start reading the DEM.
+	GDALDataset  *poDataset;
+	GDALAllRegister();
+	poDataset = (GDALDataset*)GDALOpen(infile.c_str(), GA_ReadOnly);
+	if(poDataset == NULL)
+	{
+		cout << "There was a problem opening the topography file.\n";
+		return 1;
+	}
+
+	double	adfGeoTransform[6], origin_x, origin_y;
+	int		cellsX = poDataset->GetRasterXSize(),
+			cellsY = poDataset->GetRasterYSize(),
+			layers = poDataset->GetRasterCount(),
+			physicalSize,zone;
 
     if( poDataset->GetProjectionRef()  != NULL )
         printf( "Projection is `%s'\n", poDataset->GetProjectionRef() );
@@ -91,12 +166,12 @@ int main(int argc, char* argv[])
         pafScanline = (float *) CPLMalloc(sizeof(float)*nXSize*nYSize);
         poBand->RasterIO( GF_Read, 0, 0, nXSize, nYSize, pafScanline, nXSize, nYSize, GDT_Float32, 0, 0 );
 
-		for(int yp = 0; yp<nYSize; yp++)
+		/*for(int yp = 0; yp<nYSize; yp++)
 		{
 			for(int xp = 0; xp<nXSize; xp++)
 				cout << pafScanline[yp * nXSize + xp] << ",\t";
 			cout << endl;
-		}			  
+		}*/			  
 					  
 					  
 					  
