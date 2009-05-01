0e65dab72f7dae517d68506448527b8873edf11b
Anthony Heathcoat <aeheathc@gmail.com>
2009-05-01 02:21:17 +0000

    Added GPL notice to files
    git-svn-id: http://svn.code.sf.net/p/laharplot/code/trunk@109 95a9f313-b2ac-4aab-b0cd-30946b13c7fa

--- plotter/tsv.h
+++ plotter/tsv.h
@@ -1,10 +1,25 @@
+/* LaharPlot is an application that can calculate and map out the inundation
+   zone of a lahar (volcanic mudslide) given an elevation map and some starting
+   parameters about the lahar itself.
+   Copyright 2009: Anthony Heathcoat, Jason Anderson, Tim Root
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /***************************************************************
  * Name:      tsv.h
  * Purpose:   Handles tsv files, and functions
- * Author:     ()
- * Created:   2009-03-16
- * Copyright:  ()
- * License:
  **************************************************************/
 
 #ifndef TSV_H
